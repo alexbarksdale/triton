10459cef42d261c2ef2b1772cbaaff881fe9a937
alexbarksdale <ale.barksdale@gmail.com>
2020-04-11 17:22:09 -0500

    [Add] Moved get and set out of user

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -1953,7 +1953,34 @@ function () {
 }();
 
 exports.Sync = Sync;
-},{"axios":"node_modules/axios/index.js"}],"src/models/User.ts":[function(require,module,exports) {
+},{"axios":"node_modules/axios/index.js"}],"src/models/Attributes.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var Attributes =
+/** @class */
+function () {
+  function Attributes(data) {
+    this.data = data;
+  } // extends is limiting the values K can have to what T has
+
+
+  Attributes.prototype.get = function (key) {
+    return this.data[key];
+  };
+
+  Attributes.prototype.set = function (update) {
+    Object.assign(this.data, update);
+  };
+
+  return Attributes;
+}();
+
+exports.Attributes = Attributes;
+},{}],"src/models/User.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -1964,30 +1991,24 @@ var Eventing_1 = require("./Eventing");
 
 var Sync_1 = require("./Sync");
 
+var Attributes_1 = require("./Attributes");
+
 var ROOT_URL = 'http://localhost:3000/users';
 
 var User =
 /** @class */
 function () {
-  function User(data) {
-    this.data = data;
+  function User(attrs) {
     this.events = new Eventing_1.Eventing();
     this.sync = new Sync_1.Sync(ROOT_URL);
+    this.attributes = new Attributes_1.Attributes(attrs);
   }
-
-  User.prototype.get = function (propName) {
-    return this.data[propName];
-  };
-
-  User.prototype.set = function (update) {
-    Object.assign(this.data, update);
-  };
 
   return User;
 }();
 
 exports.User = User;
-},{"./Eventing":"src/models/Eventing.ts","./Sync":"src/models/Sync.ts"}],"src/index.ts":[function(require,module,exports) {
+},{"./Eventing":"src/models/Eventing.ts","./Sync":"src/models/Sync.ts","./Attributes":"src/models/Attributes.ts"}],"src/index.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
---
+++ src/models/Attributes.ts
@@ -0,0 +1,12 @@
+export class Attributes<T> {
+    constructor(private data: T) {}
+
+    // extends is limiting the values K can have to what T has
+    get<K extends keyof T>(key: K): T[K] {
+        return this.data[key];
+    }
+
+    set(update: T): void {
+        Object.assign(this.data, update);
+    }
+}
